bcc7ef996f27f9ce1aced64367f0fa50e4b3245a
Manoel Tindão <timdao02@hotmail.com>
2020-04-28 20:14:33 -0300

    Adicionado NavBar do título

--- src/main/java/com/tindao/osworks/controller/TituloController.java
+++ src/main/java/com/tindao/osworks/controller/TituloController.java
@@ -1,12 +1,14 @@
 package com.tindao.osworks.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TituloController 
 {
+	@RequestMapping("/titulos/novo")
 	public String novo()
 	{
-		return "CadastroTitulo";
+		return ("CadastroTitulo");
 	}
 }
