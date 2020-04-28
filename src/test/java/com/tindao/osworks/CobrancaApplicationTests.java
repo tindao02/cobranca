323ba653495cff5aa64c136493bcb0eb490a957f
Manoel Tindão <timdao02@hotmail.com>
2020-04-28 17:12:26 -0300

    Adicionar ao Git

---
+++ src/test/java/com/tindao/osworks/CobrancaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tindao.osworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobrancaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
