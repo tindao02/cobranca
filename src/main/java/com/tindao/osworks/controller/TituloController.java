0b745fe91488685c40dce430c134ecb28853468e
Manoel Tindão <timdao02@hotmail.com>
2020-04-28 18:39:20 -0300

    Adicionado o bootstrap e classe controller título

---
+++ src/main/java/com/tindao/osworks/controller/TituloController.java
@@ -0,0 +1,12 @@
+package com.tindao.osworks.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TituloController 
+{
+	public String novo()
+	{
+		return "CadastroTitulo";
+	}
+}
