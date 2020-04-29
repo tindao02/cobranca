6527f1d83c431c9dcaf7879f291e96aaaef85fa0
Manoel Tindão <timdao02@hotmail.com>
2020-04-29 19:31:51 -0300

    Campos aceitam data formato brasil e casa decimal com virgula

--- src/main/java/com/tindao/osworks/model/Titulo.java
+++ src/main/java/com/tindao/osworks/model/Titulo.java
@@ -12,6 +12,9 @@ import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
+
 @Entity
 public class Titulo 
 {	
@@ -22,8 +25,10 @@ public class Titulo
 	private String descricao;
 	
 	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern ="dd/MM/yyyy")
 	private Date dataVencimento;
 	
+	@NumberFormat(pattern = "#,##0.00")
 	private BigDecimal valor;
 	
 	@Enumerated(EnumType.STRING)
