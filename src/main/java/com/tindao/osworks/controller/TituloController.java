629ed362b8f9569e7d0ee381706123d7fcc40719
Manoel Tindão <timdao02@hotmail.com>
2020-05-01 15:43:11 -0300

    Adicionado o aleta de mensagem salva

--- src/main/java/com/tindao/osworks/controller/TituloController.java
+++ src/main/java/com/tindao/osworks/controller/TituloController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.tindao.osworks.model.Titulo;
 import com.tindao.osworks.repository.Titulos;
@@ -11,21 +12,31 @@ import com.tindao.osworks.repository.Titulos;
 @Controller
 @RequestMapping("/titulos")
 public class TituloController 
-{
+{	
+	//Repositório
 	@Autowired
 	private Titulos titulos;
 	
+	/*
+	 * Método para chamar a página web
+	 */
 	@RequestMapping("/novo")
 	public String novo()
 	{
-		return ("CadastroTitulo");
+		return ("CadastroTitulo"); //retorma o nome da página que será carregada
 	}
 	
-	@RequestMapping(method = RequestMethod.POST)
-	public String salvar(Titulo titulo)
+	/*
+	 * Metodo para salvar 
+	 */
+	@RequestMapping(method = RequestMethod.POST)	//informa o tipo de recebimento das informações
+	public ModelAndView salvar(Titulo titulo)
 	{
 		titulos.save(titulo);
 		
-		return ("CadastroTitulo");
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("mensagem", "Título salvo com sucesso!");
+		
+		return (mv);
 	}
 }
